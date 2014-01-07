243f04944a06ced5e3b0b852b8b6379f11e1605c
RedKite Labs <info@redkite-labs.com>
2014-01-07 13:27:51 +0100

    upgraded theme to new design

--- DependencyInjection/SunshineThemeExtension.php
+++ DependencyInjection/SunshineThemeExtension.php
@@ -4,12 +4,12 @@
  * under the MIT License. In addiction, to use this bundle, you must leave
  * intact this copyright notice.
  *
- * (c) Since 2011 RedKiteCms
+ * Copyright (c) RedKite Labs <webmaster@redkite-labs.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
- * For extra documentation and help please visit http://alphalemon.com
+ * For extra documentation and help please visit http://www.redkite-labs.com
  *
  * @license    GPL LICENSE Version 2.0
  *
@@ -25,37 +25,22 @@ class SunshineThemeExtension extends BaseExtension
     {
         return
             array(
+                'path' => __DIR__.'/../Resources/config',
+                'theme' =>
                 array(
-                    'path' => __DIR__.'/../Resources/config',
-                    'configFiles' =>
-                    array(
-                        'sunshine_theme.xml',
-                    ),
-                    'configuration' =>
-                    array(
-                        array(
-                            'path' => __DIR__.'/../Resources/config/templates',
-                            'configFiles' =>
-                            array(
-                                'home.xml',
-                                'internal.xml',
-                            ),
-                            'configuration' =>
-                            array(
-                                array(
-                                    'path' => __DIR__.'/../Resources/config/templates/slots',
-                                    'configFiles' =>
-                                    array(
-                                        'home.xml',
-                                        'base.xml',
-                                        'internal.xml',
-                                    ),
-                                ),
-                            ),
-                        ),
-                    ),
+                    'sunshine_theme.xml',
                 ),
-            );
+                'templates' =>
+                array(
+                    'templates/internal.xml',
+                    'templates/home.xml',
+                ),
+                'slots' =>
+                array(
+                    'slots/slots.xml',
+                ),
+            )
+        ;
     }
 
     public function getAlias()
